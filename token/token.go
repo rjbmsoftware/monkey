1e2a8664982e3b62f2595b0d1da9ca44d5d53e68
richard.millett <richard.millett+github@googlemail.com>
2025-03-07 15:45:39 +0000

    wip lexer

--- token/token.go
+++ token/token.go
@@ -13,8 +13,14 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
